439e19a926dade6c7c51e020d921f10908c3db8d
服部隆志 <hattori@MacBook-Air-M1-3.local>
2022-12-02 01:03:32 +0900

    course model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_154412) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_155632) do
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.integer "credit"
+    t.boolean "compulsory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "faculties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
