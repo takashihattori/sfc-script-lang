a7aff269843ba99be5deec8ef49c1b887adee163
服部隆志 <hattori@MacBook-Air-M1-3.local>
2022-12-16 03:23:50 +0900

    form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Faculty.create(name: '総合政策')
+Faculty.create(name: '環境情報')
+Course.create(name: '体育１', credit: 1, compulsory: true)
+Course.create(name: 'プログラミング言語論', credit: 2, compulsory: false)
