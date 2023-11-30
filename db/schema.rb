40e41c8719bbda8108120b6a04e941b869259f68
服部隆志 <hattori@MacBookAirM2.local>
2023-12-01 01:45:36 +0900

    課題11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_162950) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_163311) do
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.integer "credit"
+    t.boolean "compulsory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "faculties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
