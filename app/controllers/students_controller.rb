813af01673c4ac2688dfe5ab59f0298aa0098ff5
服部隆志 <hattori@Mac-mini-kappa202.local>
2025-01-15 18:55:55 +0900

    10.フォーム

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -65,6 +65,6 @@ class StudentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def student_params
-      params.require(:student).permit(:name, :faculty_id)
+      params.require(:student).permit(:name, :faculty_id, :grade)
     end
 end
