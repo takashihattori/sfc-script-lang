15a802f8ad801356799fe07a7de9802ded1c7760
服部隆志 <hattori@Mac-mini-kappa202.local>
2025-01-15 18:48:06 +0900

    9.コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :courses, only: [ :index, :show ]
+  resources :faculties, only: [ :show ]
   resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
